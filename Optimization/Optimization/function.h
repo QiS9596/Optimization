362ba0bfb03eff657b1d826e4691ee78a436d8d3
Rolo <rolo_lamperouge@foxmail.com>
2016-05-22 09:48:51 +0800

    change to project without shader

---
+++ Optimization/Optimization/function.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <vector>
+#define vecD std::vector<double>
+#include <iostream>
+class function
+{
+public:
+	function();
+	virtual double operator()(vecD);
+	void df(vecD & x, vecD & deriv);
+};
+
+class testFunc1 : public function {
+public:
+	double operator()(vecD x) {
+		return x[0] * x[0] + x[1] * x[1];
+	}
+	void standardDF(vecD &x, vecD & deriv) {
+		deriv.push_back(2.0*x[0]);
+		deriv.push_back(2.0*x[1]);
+	}
+};
+
+class testFunc2 : public function {
+public:
+	double operator()(double x) { return (x + 1)*(x + 1)+1; };
+};
