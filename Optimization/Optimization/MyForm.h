4b7375449277c9a26d7205d84e6b0d0ca396512e
Rolo <rolo_lamperouge@foxmail.com>
2016-05-23 20:24:28 +0800

    start to do input handling
    trying to handle input

--- Optimization/Optimization/MyForm.h
+++ Optimization/Optimization/MyForm.h
@@ -24,6 +24,7 @@ namespace Optimization {
 	public ref class MyForm : public System::Windows::Forms::Form
 	{
 	public:
+		std::vector<function*> * functions;
 		MyForm(void)
 		{
 			InitializeComponent();
@@ -31,12 +32,18 @@ namespace Optimization {
 			//
 			//TODO:  在此加入建構函式程式碼
 			//
-
+			functions = new std::vector<function*>;
+			testFunc1 *func01 = new testFunc1();
+			testFunc2 *func02 = new testFunc2();
+			testFunc3 *func03 = new testFunc3();
+			(*functions).push_back(func01);
+			(*functions).push_back(func02);
+			(*functions).push_back(func03);
 			//=============================================
 			//test space
 			std::cout << "Quasi-Newton Method test" << std::endl;
 			testFunc1 func0;
-			Qnewton qnw(func0);
+			Qnewton qnw((*((*functions)[0])));
 			VecD myp; myp.push_back(0.3);
 			myp = qnw.minimize(myp);
 
@@ -119,11 +126,12 @@ namespace Optimization {
 			this->tableLayoutPanel1->Controls->Add(this->menuStrip1, 0, 0);
 			this->tableLayoutPanel1->Dock = System::Windows::Forms::DockStyle::Fill;
 			this->tableLayoutPanel1->Location = System::Drawing::Point(0, 0);
+			this->tableLayoutPanel1->Margin = System::Windows::Forms::Padding(4);
 			this->tableLayoutPanel1->Name = L"tableLayoutPanel1";
 			this->tableLayoutPanel1->RowCount = 2;
 			this->tableLayoutPanel1->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Percent, 5)));
 			this->tableLayoutPanel1->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Percent, 95)));
-			this->tableLayoutPanel1->Size = System::Drawing::Size(484, 609);
+			this->tableLayoutPanel1->Size = System::Drawing::Size(726, 914);
 			this->tableLayoutPanel1->TabIndex = 0;
 			// 
 			// flowLayoutPanel1
@@ -133,51 +141,60 @@ namespace Optimization {
 			this->flowLayoutPanel1->Controls->Add(this->OutputText);
 			this->flowLayoutPanel1->Controls->Add(this->Output);
 			this->flowLayoutPanel1->Dock = System::Windows::Forms::DockStyle::Fill;
-			this->flowLayoutPanel1->Location = System::Drawing::Point(3, 33);
+			this->flowLayoutPanel1->Location = System::Drawing::Point(4, 49);
+			this->flowLayoutPanel1->Margin = System::Windows::Forms::Padding(4);
 			this->flowLayoutPanel1->Name = L"flowLayoutPanel1";
-			this->flowLayoutPanel1->Size = System::Drawing::Size(478, 573);
+			this->flowLayoutPanel1->Size = System::Drawing::Size(718, 861);
 			this->flowLayoutPanel1->TabIndex = 0;
 			// 
 			// InputText
 			// 
 			this->InputText->AutoSize = true;
-			this->InputText->Location = System::Drawing::Point(3, 0);
+			this->InputText->Location = System::Drawing::Point(4, 0);
+			this->InputText->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->InputText->Name = L"InputText";
-			this->InputText->Size = System::Drawing::Size(30, 12);
+			this->InputText->Size = System::Drawing::Size(43, 18);
 			this->InputText->TabIndex = 0;
 			this->InputText->Text = L"Input";
+			this->InputText->Click += gcnew System::EventHandler(this, &MyForm::InputText_Click);
 			// 
 			// Input
 			// 
-			this->Input->Location = System::Drawing::Point(3, 15);
+			this->Input->Location = System::Drawing::Point(4, 22);
+			this->Input->Margin = System::Windows::Forms::Padding(4);
 			this->Input->Multiline = true;
 			this->Input->Name = L"Input";
-			this->Input->Size = System::Drawing::Size(475, 132);
+			this->Input->Size = System::Drawing::Size(710, 196);
 			this->Input->TabIndex = 1;
+			this->Input->TextChanged += gcnew System::EventHandler(this, &MyForm::Input_TextChanged);
 			// 
 			// OutputText
 			// 
 			this->OutputText->AutoSize = true;
-			this->OutputText->Location = System::Drawing::Point(3, 150);
+			this->OutputText->Location = System::Drawing::Point(4, 222);
+			this->OutputText->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->OutputText->Name = L"OutputText";
-			this->OutputText->Size = System::Drawing::Size(37, 12);
+			this->OutputText->Size = System::Drawing::Size(54, 18);
 			this->OutputText->TabIndex = 2;
 			this->OutputText->Text = L"Output";
 			// 
 			// Output
 			// 
-			this->Output->Location = System::Drawing::Point(3, 165);
+			this->Output->Location = System::Drawing::Point(4, 244);
+			this->Output->Margin = System::Windows::Forms::Padding(4);
 			this->Output->Multiline = true;
 			this->Output->Name = L"Output";
-			this->Output->Size = System::Drawing::Size(475, 399);
+			this->Output->Size = System::Drawing::Size(710, 596);
 			this->Output->TabIndex = 3;
 			// 
 			// menuStrip1
 			// 
+			this->menuStrip1->ImageScalingSize = System::Drawing::Size(24, 24);
 			this->menuStrip1->Items->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(1) { this->fileToolStripMenuItem });
 			this->menuStrip1->Location = System::Drawing::Point(0, 0);
 			this->menuStrip1->Name = L"menuStrip1";
-			this->menuStrip1->Size = System::Drawing::Size(484, 24);
+			this->menuStrip1->Padding = System::Windows::Forms::Padding(9, 3, 0, 3);
+			this->menuStrip1->Size = System::Drawing::Size(726, 33);
 			this->menuStrip1->TabIndex = 1;
 			this->menuStrip1->Text = L"menuStrip1";
 			// 
@@ -185,13 +202,13 @@ namespace Optimization {
 			// 
 			this->fileToolStripMenuItem->DropDownItems->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(1) { this->loadEquationsToolStripMenuItem });
 			this->fileToolStripMenuItem->Name = L"fileToolStripMenuItem";
-			this->fileToolStripMenuItem->Size = System::Drawing::Size(39, 20);
+			this->fileToolStripMenuItem->Size = System::Drawing::Size(51, 27);
 			this->fileToolStripMenuItem->Text = L"File";
 			// 
 			// loadEquationsToolStripMenuItem
 			// 
 			this->loadEquationsToolStripMenuItem->Name = L"loadEquationsToolStripMenuItem";
-			this->loadEquationsToolStripMenuItem->Size = System::Drawing::Size(164, 22);
+			this->loadEquationsToolStripMenuItem->Size = System::Drawing::Size(223, 30);
 			this->loadEquationsToolStripMenuItem->Text = L"Load Equations";
 			this->loadEquationsToolStripMenuItem->Click += gcnew System::EventHandler(this, &MyForm::loadEquationsToolStripMenuItem_Click);
 			// 
@@ -202,11 +219,12 @@ namespace Optimization {
 			// 
 			// MyForm
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(6, 12);
+			this->AutoScaleDimensions = System::Drawing::SizeF(9, 18);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(484, 609);
+			this->ClientSize = System::Drawing::Size(726, 914);
 			this->Controls->Add(this->tableLayoutPanel1);
 			this->MainMenuStrip = this->menuStrip1;
+			this->Margin = System::Windows::Forms::Padding(4);
 			this->Name = L"MyForm";
 			this->Text = L"Optimization";
 			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
@@ -243,6 +261,35 @@ private: System::Void openFileDialog1_FileOk(System::Object^  sender, System::Co
 			Output->Text += Environment::NewLine;
 		}
 	}
+}
+private: System::Void Input_TextChanged(System::Object^  sender, System::EventArgs^  e) {
+	if (Input->Text->EndsWith("\n"))
+	{
+		String^ userInput = "";
+		array<String^> ^userCommand1 = Input->Text->Split('\n');
+		userInput = userCommand1[userCommand1->Length - 2];
+		array<String^> ^userCommand = userInput->Split(' ');
+		for (int i = 1; i < userCommand->Length; i++)
+			userCommand[0] += userCommand[i];
+		if (userCommand[0]->Length > 1) {
+			if (userCommand[0] == "clr\r"||userCommand[0] == "clr")
+			{
+				Input->Text = "";
+				Output->Text = "";
+			}
+			else
+			{
+				Output->Text += userCommand[0];
+				Output->Text += Environment::NewLine;
+				//std::vector<std::string> temp = tokenmanger->token_interface(userCommand[0]);
+				//for (int i = 0; i < temp.size(); i++)
+				//	Output->Text += gcnew String(temp[i].c_str()) + Environment::NewLine;
+			}
+		}
+	
+	}
+}
+private: System::Void InputText_Click(System::Object^  sender, System::EventArgs^  e) {
 }
 };
 }
