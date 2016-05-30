b57290c0ce77f4576d63b2a01de8293bae27030f
Inndy <inndy.tw@gmail.com>
2016-05-30 19:10:53 +0800

    Better layout and font

--- Optimization/Optimization/MyForm.h
+++ Optimization/Optimization/MyForm.h
@@ -68,6 +68,17 @@ namespace Optimization {
 			p = nw.minimize(p);
 			std::cout << p[0] << " " << p[1] << std::endl;*/
 		}
+	private: System::Windows::Forms::MenuStrip^  menuStrip1;
+	public:
+	private: System::Windows::Forms::ToolStripMenuItem^  fileToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^  loadEquationsToolStripMenuItem;
+	private: System::Windows::Forms::GroupBox^  groupBox1;
+	private: System::Windows::Forms::TextBox^  Input;
+	private: System::Windows::Forms::GroupBox^  groupBox2;
+	private: System::Windows::Forms::TextBox^  Output;
+
+	public:
+
 
 	protected:
 		/// <summary>
@@ -81,16 +92,16 @@ namespace Optimization {
 				delete components;
 			}
 		}
-	private: System::Windows::Forms::TableLayoutPanel^  tableLayoutPanel1;
+
 	protected:
-	private: System::Windows::Forms::FlowLayoutPanel^  flowLayoutPanel1;
-	private: System::Windows::Forms::Label^  InputText;
-	private: System::Windows::Forms::TextBox^  Input;
-	private: System::Windows::Forms::Label^  OutputText;
-	private: System::Windows::Forms::TextBox^  Output;
-	private: System::Windows::Forms::MenuStrip^  menuStrip1;
-	private: System::Windows::Forms::ToolStripMenuItem^  fileToolStripMenuItem;
-	private: System::Windows::Forms::ToolStripMenuItem^  loadEquationsToolStripMenuItem;
+
+
+
+
+
+
+
+
 	private: System::Windows::Forms::OpenFileDialog^  openFileDialog1;
 
 	private:
@@ -106,90 +117,23 @@ namespace Optimization {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->tableLayoutPanel1 = (gcnew System::Windows::Forms::TableLayoutPanel());
-			this->flowLayoutPanel1 = (gcnew System::Windows::Forms::FlowLayoutPanel());
-			this->InputText = (gcnew System::Windows::Forms::Label());
-			this->Input = (gcnew System::Windows::Forms::TextBox());
-			this->OutputText = (gcnew System::Windows::Forms::Label());
-			this->Output = (gcnew System::Windows::Forms::TextBox());
+			this->openFileDialog1 = (gcnew System::Windows::Forms::OpenFileDialog());
 			this->menuStrip1 = (gcnew System::Windows::Forms::MenuStrip());
 			this->fileToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
 			this->loadEquationsToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
-			this->openFileDialog1 = (gcnew System::Windows::Forms::OpenFileDialog());
-			this->tableLayoutPanel1->SuspendLayout();
-			this->flowLayoutPanel1->SuspendLayout();
+			this->groupBox1 = (gcnew System::Windows::Forms::GroupBox());
+			this->Input = (gcnew System::Windows::Forms::TextBox());
+			this->groupBox2 = (gcnew System::Windows::Forms::GroupBox());
+			this->Output = (gcnew System::Windows::Forms::TextBox());
 			this->menuStrip1->SuspendLayout();
+			this->groupBox1->SuspendLayout();
+			this->groupBox2->SuspendLayout();
 			this->SuspendLayout();
 			// 
-			// tableLayoutPanel1
+			// openFileDialog1
 			// 
-			this->tableLayoutPanel1->ColumnCount = 1;
-			this->tableLayoutPanel1->ColumnStyles->Add((gcnew System::Windows::Forms::ColumnStyle(System::Windows::Forms::SizeType::Percent,
-				100)));
-			this->tableLayoutPanel1->Controls->Add(this->flowLayoutPanel1, 0, 1);
-			this->tableLayoutPanel1->Controls->Add(this->menuStrip1, 0, 0);
-			this->tableLayoutPanel1->Dock = System::Windows::Forms::DockStyle::Fill;
-			this->tableLayoutPanel1->Location = System::Drawing::Point(0, 0);
-			this->tableLayoutPanel1->Margin = System::Windows::Forms::Padding(4);
-			this->tableLayoutPanel1->Name = L"tableLayoutPanel1";
-			this->tableLayoutPanel1->RowCount = 2;
-			this->tableLayoutPanel1->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Percent, 5)));
-			this->tableLayoutPanel1->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Percent, 95)));
-			this->tableLayoutPanel1->Size = System::Drawing::Size(726, 914);
-			this->tableLayoutPanel1->TabIndex = 0;
-			// 
-			// flowLayoutPanel1
-			// 
-			this->flowLayoutPanel1->Controls->Add(this->InputText);
-			this->flowLayoutPanel1->Controls->Add(this->Input);
-			this->flowLayoutPanel1->Controls->Add(this->OutputText);
-			this->flowLayoutPanel1->Controls->Add(this->Output);
-			this->flowLayoutPanel1->Dock = System::Windows::Forms::DockStyle::Fill;
-			this->flowLayoutPanel1->Location = System::Drawing::Point(4, 49);
-			this->flowLayoutPanel1->Margin = System::Windows::Forms::Padding(4);
-			this->flowLayoutPanel1->Name = L"flowLayoutPanel1";
-			this->flowLayoutPanel1->Size = System::Drawing::Size(718, 861);
-			this->flowLayoutPanel1->TabIndex = 0;
-			// 
-			// InputText
-			// 
-			this->InputText->AutoSize = true;
-			this->InputText->Location = System::Drawing::Point(4, 0);
-			this->InputText->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
-			this->InputText->Name = L"InputText";
-			this->InputText->Size = System::Drawing::Size(43, 18);
-			this->InputText->TabIndex = 0;
-			this->InputText->Text = L"Input";
-			this->InputText->Click += gcnew System::EventHandler(this, &MyForm::InputText_Click);
-			// 
-			// Input
-			// 
-			this->Input->Location = System::Drawing::Point(4, 22);
-			this->Input->Margin = System::Windows::Forms::Padding(4);
-			this->Input->Multiline = true;
-			this->Input->Name = L"Input";
-			this->Input->Size = System::Drawing::Size(710, 196);
-			this->Input->TabIndex = 1;
-			this->Input->TextChanged += gcnew System::EventHandler(this, &MyForm::Input_TextChanged);
-			// 
-			// OutputText
-			// 
-			this->OutputText->AutoSize = true;
-			this->OutputText->Location = System::Drawing::Point(4, 222);
-			this->OutputText->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
-			this->OutputText->Name = L"OutputText";
-			this->OutputText->Size = System::Drawing::Size(54, 18);
-			this->OutputText->TabIndex = 2;
-			this->OutputText->Text = L"Output";
-			// 
-			// Output
-			// 
-			this->Output->Location = System::Drawing::Point(4, 244);
-			this->Output->Margin = System::Windows::Forms::Padding(4);
-			this->Output->Multiline = true;
-			this->Output->Name = L"Output";
-			this->Output->Size = System::Drawing::Size(710, 596);
-			this->Output->TabIndex = 3;
+			this->openFileDialog1->FileName = L"openFileDialog1";
+			this->openFileDialog1->FileOk += gcnew System::ComponentModel::CancelEventHandler(this, &MyForm::openFileDialog1_FileOk);
 			// 
 			// menuStrip1
 			// 
@@ -197,48 +141,90 @@ namespace Optimization {
 			this->menuStrip1->Items->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(1) { this->fileToolStripMenuItem });
 			this->menuStrip1->Location = System::Drawing::Point(0, 0);
 			this->menuStrip1->Name = L"menuStrip1";
-			this->menuStrip1->Padding = System::Windows::Forms::Padding(9, 3, 0, 3);
-			this->menuStrip1->Size = System::Drawing::Size(726, 33);
-			this->menuStrip1->TabIndex = 1;
+			this->menuStrip1->Size = System::Drawing::Size(449, 24);
+			this->menuStrip1->TabIndex = 4;
 			this->menuStrip1->Text = L"menuStrip1";
 			// 
 			// fileToolStripMenuItem
 			// 
 			this->fileToolStripMenuItem->DropDownItems->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(1) { this->loadEquationsToolStripMenuItem });
 			this->fileToolStripMenuItem->Name = L"fileToolStripMenuItem";
-			this->fileToolStripMenuItem->Size = System::Drawing::Size(51, 27);
+			this->fileToolStripMenuItem->Size = System::Drawing::Size(39, 20);
 			this->fileToolStripMenuItem->Text = L"File";
 			// 
 			// loadEquationsToolStripMenuItem
 			// 
 			this->loadEquationsToolStripMenuItem->Name = L"loadEquationsToolStripMenuItem";
-			this->loadEquationsToolStripMenuItem->Size = System::Drawing::Size(223, 30);
+			this->loadEquationsToolStripMenuItem->Size = System::Drawing::Size(164, 22);
 			this->loadEquationsToolStripMenuItem->Text = L"Load Equations";
 			this->loadEquationsToolStripMenuItem->Click += gcnew System::EventHandler(this, &MyForm::loadEquationsToolStripMenuItem_Click);
 			// 
-			// openFileDialog1
+			// groupBox1
 			// 
-			this->openFileDialog1->FileName = L"openFileDialog1";
-			this->openFileDialog1->FileOk += gcnew System::ComponentModel::CancelEventHandler(this, &MyForm::openFileDialog1_FileOk);
+			this->groupBox1->Controls->Add(this->Input);
+			this->groupBox1->Dock = System::Windows::Forms::DockStyle::Top;
+			this->groupBox1->Location = System::Drawing::Point(0, 24);
+			this->groupBox1->Name = L"groupBox1";
+			this->groupBox1->Padding = System::Windows::Forms::Padding(5, 3, 5, 5);
+			this->groupBox1->Size = System::Drawing::Size(449, 167);
+			this->groupBox1->TabIndex = 5;
+			this->groupBox1->TabStop = false;
+			this->groupBox1->Text = L"Input";
+			// 
+			// Input
+			// 
+			this->Input->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->Input->Font = (gcnew System::Drawing::Font(L"Consolas", 9.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->Input->Location = System::Drawing::Point(5, 18);
+			this->Input->Multiline = true;
+			this->Input->Name = L"Input";
+			this->Input->Size = System::Drawing::Size(439, 144);
+			this->Input->TabIndex = 1;
+			this->Input->TextChanged += gcnew System::EventHandler(this, &MyForm::Input_TextChanged);
+			// 
+			// groupBox2
+			// 
+			this->groupBox2->Controls->Add(this->Output);
+			this->groupBox2->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->groupBox2->Location = System::Drawing::Point(0, 191);
+			this->groupBox2->Name = L"groupBox2";
+			this->groupBox2->Padding = System::Windows::Forms::Padding(5, 3, 5, 5);
+			this->groupBox2->Size = System::Drawing::Size(449, 338);
+			this->groupBox2->TabIndex = 6;
+			this->groupBox2->TabStop = false;
+			this->groupBox2->Text = L"Output";
+			// 
+			// Output
+			// 
+			this->Output->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->Output->Font = (gcnew System::Drawing::Font(L"Consolas", 9.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->Output->Location = System::Drawing::Point(5, 18);
+			this->Output->Multiline = true;
+			this->Output->Name = L"Output";
+			this->Output->Size = System::Drawing::Size(439, 315);
+			this->Output->TabIndex = 3;
 			// 
 			// MyForm
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(9, 18);
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 12);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(726, 914);
-			this->Controls->Add(this->tableLayoutPanel1);
-			this->MainMenuStrip = this->menuStrip1;
-			this->Margin = System::Windows::Forms::Padding(4);
+			this->ClientSize = System::Drawing::Size(449, 529);
+			this->Controls->Add(this->groupBox2);
+			this->Controls->Add(this->groupBox1);
+			this->Controls->Add(this->menuStrip1);
 			this->Name = L"MyForm";
 			this->Text = L"Optimization";
 			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
-			this->tableLayoutPanel1->ResumeLayout(false);
-			this->tableLayoutPanel1->PerformLayout();
-			this->flowLayoutPanel1->ResumeLayout(false);
-			this->flowLayoutPanel1->PerformLayout();
 			this->menuStrip1->ResumeLayout(false);
 			this->menuStrip1->PerformLayout();
+			this->groupBox1->ResumeLayout(false);
+			this->groupBox1->PerformLayout();
+			this->groupBox2->ResumeLayout(false);
+			this->groupBox2->PerformLayout();
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
@@ -386,8 +372,6 @@ private: System::Void Input_TextChanged(System::Object^  sender, System::EventAr
 		}
 	
 	}
-}
-private: System::Void InputText_Click(System::Object^  sender, System::EventArgs^  e) {
 }
 };
 }
